35ea543fede442fb4dc667b281e902c15166a7eb
Mandine44 <amandine.huret@gmail.com>
2021-12-09 14:09:35 +0100

    commit1

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,22 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ byte micro = 2;
+ short petit =12;
+ int grand = 500;
+ long énorme = 1000_000;
+ float reel = 1.5f;
+ double GrosReel = 200_000.18;
+ char caractere = 'a';
+ boolean Vrai = true;
+ String Bonjour = "Hello";
+ String randomString = "Voici le resultat d'un calcul : \n 1+5 =6";
+ 
+ System.out.println(randomString);
+ 
+	}
+
+}
