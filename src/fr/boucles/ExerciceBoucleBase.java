1487a6765b15cb4d14d915a38418d60c8dbf8d7e
Mandine44 <amandine.huret@gmail.com>
2021-12-09 14:49:05 +0100

    commit2

---
+++ src/fr/boucles/ExerciceBoucleBase.java
@@ -0,0 +1,22 @@
+package fr.boucles;
+
+public class ExerciceBoucleBase {
+
+	public static void main(String[] args) {
+		for (int i = 1; i<=10; i++) {
+			System.out.println(i);
+		}
+		for (int i =1; i<=20; i++) {
+			System.out.println("Amandine Huret");
+		}
+		for (int i =2; i<=100; i+=2) {
+			System.out.println(i);
+		}
+		for (int i =1; i<=99; i+=2) {
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
---
+++ src/fr/boucles/ExerciceTableauEntier.java
@@ -0,0 +1,14 @@
+package fr.boucles;
+
+public class ExerciceTableauEntier {
+
+	public static void main(String[] args) {
+	int tabEntiers[]={1,2,3,4,5,6,7,8,9,10};
+	System.out.println(tabEntiers[0]);
+	System.out.println(tabEntiers.length);
+	System.out.println(tabEntiers[tabEntiers.length-1]);
+	tabEntiers[4]=8;
+	System.out.println(tabEntiers[4]);
+	}
+
+}
